a09870d56661059a6c42d23149ecf3f97180565d
alan-6565 <153258467+alan-6565@users.noreply.github.com>
2025-11-19 10:07:02 -0800

    new commit

---
+++ 01_Products_list.sql
@@ -0,0 +1,3 @@
+-- the table that holds the items is Products
+SELECT ProductID, ProductName, UnitPrice
+FROM northwind.Products;
--- Untitled.sql
+++ Untitled.sql
@@ -1,2 +1,3 @@
+-- the table that hold the items is northwind.Products
 SELECT *
 FROM northwind.Orders;
